2774233817487f82c1c3c442b9adbe9e95bf4072
vassaneer <pearitono@gmail.com>
2021-03-15 19:10:10 +0700

    15/3/64

---
+++ Pet_se/Assets/Script/Animate.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+    float TimeInterval;
+    public CharacterController2D controller;
+    public bool trigger;
+    public float speed = 1000f;
+    public float ac = 10f;
+    private float horizontalMove = 0f;
+    Animator anim;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        intervale();
+
+    }
+    void FixedUpdate()
+    {
+        anim.SetFloat("speed",horizontalMove);
+        controller.Move(horizontalMove*Time.fixedDeltaTime, false, false);
+    }
+    void intervale()
+    {
+        TimeInterval += 1;
+        if (TimeInterval >= 0 & TimeInterval <= 1000)
+        {
+            horizontalMove = speed;
+        }
+        if (TimeInterval >= 1000 & TimeInterval <= 2000)
+        {
+            horizontalMove = 0;
+
+        }
+        if (TimeInterval >= 2000)
+        {
+            TimeInterval = 0;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "edge")
+        {
+            speed *= -1;
+        }
+        if (collision.gameObject.tag == "pet")
+        {
+            //collision.gameObject.SetActive(false);
+        }
+
+    }
+    void OnTriggerEnter(Collider trig)
+    {
+        
+    }
+
+}
---
+++ Pet_se/Assets/Script/Pet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using API;
+using UnityEngine.UI;
+using System;
+
+public class Pet : MonoBehaviour
+{
+    public Initial ini;
+    public Sprite sprite;
+    public GameObject[] go;
+    public List<Text> texts;
+    public List<Slider> hungers;
+    public List<Slider> happys;
+    public List<Text> statuss;
+    public List<Text> messages;
+    public object _queueLock = new object();
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        for (int index = 0; index < 3; index++)
+        {
+            //texts.Add(GameObject.Find("nong" + (index + 1).ToString() + "statuss").GetComponent<Text>());
+            hungers.Add(GameObject.Find("hungerSlider" + (index + 1).ToString()).GetComponent<Slider>());
+            happys.Add(GameObject.Find("happySlider" + (index + 1).ToString()).GetComponent<Slider>());
+            statuss.Add(GameObject.Find("statusText" + (index + 1).ToString()).GetComponent<Text>());
+            messages.Add(GameObject.Find("messageText" + (index + 1).ToString()).GetComponent<Text>());
+        }
+    }
+    void Start()
+    {
+        ini = new Initial("127.0.0.1",3306,"root","1234");
+    }
+    // when value in pet change
+    private void Pets_PetChange()
+    {
+          //Debug.Log("change");
+            int index = 0;
+            foreach (API.Pet pet in ini.pets.Pet)
+            {
+                //Debug.Log(pet.speak);
+                hungers[index].value = (float)pet.hungry / 100;
+                happys[index].value = (float)pet.mental / 100;
+                //texts[index].text = pet.name;
+                statuss[index].text = pet.status;
+                messages[index].text = pet.speak;
+            float position_x = (GameObject.Find("nong" + (index + 1).ToString()).GetComponent<RectTransform>()).position.x;
+                messages[index].transform.position = new Vector3(position_x, 0, 0);
+                index++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pets_PetChange();
+    }
+}
